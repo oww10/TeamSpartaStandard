3f315bd4911e7b584620b47dbb0c2606c023057d
oww10 <dntjr2209@gmail.com>
2025-08-28 16:56:51 +0900

    week1Lesson2
    실습과제입니다

--- Week1Lesson2/Week1Lesson2/ViewController.swift
+++ Week1Lesson2/Week1Lesson2/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterText: UILabel!
+    var number = 0
+    
+    
+    @IBAction func counterButton(_ sender: Any) {
+        number += 1
+        
+        counterText.text = "\(number)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        counterText.text = "\(number)"
+        
+        
     }
 
 
 }
-
