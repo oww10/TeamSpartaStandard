94136dc740db6314f77a0109f8ca610f110dc0a8
oww10 <dntjr2209@gmail.com>
2025-08-28 16:18:42 +0900

    Create stroyboard

---
+++ Week1Lesson2/Week1Lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week1Lesson2
+//
+//  Created by s s on 8/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Week1Lesson2/Week1Lesson2Tests/Week1Lesson2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Week1Lesson2Tests.swift
+//  Week1Lesson2Tests
+//
+//  Created by s s on 8/28/25.
+//
+
+import Testing
+@testable import Week1Lesson2
+
+struct Week1Lesson2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
